f8eb3e77252cb1f01c9397f35d37a1e7d0becc9f
georgeonv <george49vizx@gmail.com>
2026-01-27 19:00:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 function invertColors() {
   document.body.classList.toggle("inverted");
-
-  if (body.style.backgroundColor === "black") {
-    body.style.backgroundColor = "white";
-  } else {
-    body.style.backgroundColor = "black";
-  }
 }
 
