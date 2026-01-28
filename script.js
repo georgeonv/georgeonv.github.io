5b4363af0b8a317bd4c02e6e6c2d48c7f981674b
georgeonv <george49vizx@gmail.com>
2026-01-28 14:00:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-function invertColors() {
-  document.body.classList.toggle("inverted");
-}
-
-const button = document.querySelector("button");
-
-button.addEventListener("animationiteration", () => {
-  for (let i = 0; i <= 3; i++) {
-    document.documentElement.style.setProperty(
-      `--Cshadow${i}`,
-      Math.floor(Math.random() * 20 + 5) + "px"
-    );
-    document.documentElement.style.setProperty(
-      `--Pshadow${i}`,
-      Math.floor(Math.random() * 20 + 5) + "px"
-    );
-  }
-});
 
